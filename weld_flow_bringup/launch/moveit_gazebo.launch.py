3d82841536a8bc8135c16e872b688794ce84992d
IntoTheVoid-61 <ziga.breznikar@student.um.si>
2026-08-05 17:05:09 +0200

    Configured sim launch file for both Kuka and UR10e

--- weld_flow_bringup/launch/moveit_gazebo.launch.py
+++ weld_flow_bringup/launch/moveit_gazebo.launch.py
@@ -8,43 +8,146 @@ from launch_ros.parameter_descriptions import ParameterValue
 from moveit_configs_utils import MoveItConfigsBuilder
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
+from ament_index_python.packages import get_package_share_directory
+import yaml
+from pathlib import Path
 
 
-def generate_launch_description(robot_model="ur10e"):
+def generate_launch_description(robot_type="kuka"):
     """
     Generates launch description for MoveIt-Gazebo pipeline
     Controller manager is already started in gazebo_sim.urdf.xacro
     """
 
+    # Add launch configurations
+    declare_world_cmd = DeclareLaunchArgument(
+        name="gz_world",
+        default_value="empty",
+        choices=["empty"],
+        description="Name of world to load"
+    )
+
+    declare_x_cmd = DeclareLaunchArgument(
+        name="x",
+        default_value="0",
+    )
+
+    declare_y_cmd = DeclareLaunchArgument(
+        name="y",
+        default_value="0",
+    )
+
+    declare_z_cmd = DeclareLaunchArgument(
+        name="z",
+        default_value="0",
+    )
+
+    declare_roll_cmd = DeclareLaunchArgument(
+        name="roll",
+        default_value="0",
+    )
+
+    declare_pitch_cmd = DeclareLaunchArgument(
+        name="pitch",
+        default_value="0",
+    )
+
+    declare_yaw_cmd = DeclareLaunchArgument(
+        name="yaw",
+        default_value="0",
+    )
+
+    declare_ns_cmd = DeclareLaunchArgument(
+        name="namespace",
+        default_value="",
+    )
+
+
+
+
+    # load config params
+    bringup_config_path = (
+        Path(get_package_share_directory("weld_flow_bringup"))
+        / "config"
+        / "bringup_params.yaml"
+    )
+    with open(bringup_config_path, "r") as f:
+        config_params = yaml.load(f, Loader=yaml.SafeLoader)
+
+
+        
+
     pkg_share_gazebo = FindPackageShare('weld_flow_gazebo')
     pkg_ros_gz_sim = FindPackageShare(package='ros_gz_sim').find('ros_gz_sim')
-    pkg_share_description = FindPackageShare(f"{robot_model}_moveit_config")
 
     rviz_config_path = PathJoinSubstitution(
-        [pkg_share_description,'rviz','visualize.rviz']
+        [pkg_share_gazebo,'rviz','visualize.rviz']
     )
 
     ros_gz_bridge_config_path = PathJoinSubstitution(
         [pkg_share_gazebo,'config','ros_gz_bridge.yaml']
     )
 
-    world_path = PathJoinSubstitution(
-        [pkg_share_gazebo,'worlds','empty.world']
-    )
+    world_path = PathJoinSubstitution([
+        pkg_share_gazebo,
+        'worlds',
+        [LaunchConfiguration("gz_world"), ".world"]
+    ])
 
+    if (robot_type=="ur10e"):
+        #pkg_share_description = FindPackageShare(f"{robot_type}_moveit_config")
 
-    moveit_config = (
-        MoveItConfigsBuilder(f"{robot_model}", package_name=f"{robot_model}_moveit_config")
-        .robot_description(
-            file_path=f"config/{robot_model}.urdf.xacro",
-            mappings={
-                "use_gazebo": "true",
-                "use_mock_hardware": "false"
-            },
+        moveit_config = (
+            MoveItConfigsBuilder(f"{robot_type}", package_name=f"{robot_type}_moveit_config")
+            .robot_description(
+                file_path=f"config/{robot_type}.urdf.xacro",
+                mappings={
+                    "use_gazebo": "true",
+                    "use_mock_hardware": "false"
+                },
+            )
+            .to_moveit_configs()
         )
-        .to_moveit_configs()
-    )
 
+    elif (robot_type=="kuka"):
+        robot_model=config_params['kuka']['moveit_bringup_gazebo']['robot_model']
+        robot_family=config_params['kuka']['moveit_bringup_gazebo']['robot_family']
+
+        if(robot_family == "cybertech"): # quick fix, for preliminary dev, generalize later
+            moveit_config_pkg="kuka_kr"
+
+
+        moveit_config = (
+            MoveItConfigsBuilder(moveit_config_pkg)
+            .robot_description(
+                file_path=get_package_share_directory(f"kuka_{robot_family}_support")
+                + f"/urdf/{robot_model}.urdf.xacro",
+                mappings={
+                    "x": LaunchConfiguration("x"),
+                    "y": LaunchConfiguration("y"),
+                    "z": LaunchConfiguration("z"),
+                    "roll": LaunchConfiguration("roll"),
+                    "pitch": LaunchConfiguration("pitch"),
+                    "yaw": LaunchConfiguration("yaw"),
+                    "prefix": LaunchConfiguration("namespace"),
+                    "mode": "gazebo" # mode gazebo
+                },
+            )
+            .robot_description_semantic(
+                get_package_share_directory(f"{moveit_config_pkg}_moveit_config")
+                + f"/urdf/{robot_model}.srdf"
+            )
+            .robot_description_kinematics(file_path="config/kinematics.yaml")
+            .trajectory_execution(file_path="config/moveit_controllers.yaml")
+            .planning_scene_monitor(
+                publish_robot_description=True, publish_robot_description_semantic=True
+            )
+            .joint_limits(
+                file_path=get_package_share_directory(f"kuka_{robot_family}_support")
+                + f"/config/{robot_model}_joint_limits.yaml"
+            )
+            .to_moveit_configs()
+        )
 
 
     #--Nodes to start--#
@@ -132,16 +235,18 @@ def generate_launch_description(robot_model="ur10e"):
         output='screen',
         arguments=[
             '-topic', '/robot_description',
-            '-name', robot_model,
+            '-name', robot_type,
             '-allow_renaming', 'true',
         ])
 
-    # Load controllers
+    # Load controllers, different names for ur10e and kuka
     load_controllers = []
-    for controller in [
-        "arm_controller",
-        "joint_state_broadcaster"
-    ]:
+    if (robot_type == "ur10e"):
+        controllers=["arm_controller","joint_state_broadcaster"]
+    elif (robot_type == "kuka"):
+        controllers=["joint_trajectory_controller","joint_state_broadcaster"]
+
+    for controller in controllers:
         load_controllers += [
             ExecuteProcess(
                 cmd=["ros2 run controller_manager spawner {}".format(controller)],
@@ -152,6 +257,14 @@ def generate_launch_description(robot_model="ur10e"):
 
     return LaunchDescription(
         [
+            declare_world_cmd,
+            declare_x_cmd,
+            declare_y_cmd,
+            declare_z_cmd,
+            declare_roll_cmd,
+            declare_pitch_cmd,
+            declare_yaw_cmd,
+            declare_ns_cmd,
             start_robot_state_publisher,
             start_move_group,
             start_rviz_cmd,
